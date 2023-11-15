fd44b78c7d5bd8d58124c4414a900e1fd00128ce
Mariia-Kadochnikova <kadochnikova.mariia@gmail.com>
2023-11-15 12:47:03 +0100

    Done version Raster data
    sdndt_Sim_LIP_dPul_NDT_make_raster.m code version with edits made

--- sdndt_Sim_LIP_dPul_NDT_make_raster.m
+++ sdndt_Sim_LIP_dPul_NDT_make_raster.m
@@ -16,19 +16,16 @@ target_state = 6; % 6 - cue on , 4 - target acquisition
 
 switch target_state
     case 6
-        target_state_name = "cueON";
+        target_state_name = 'cueON';
     case 4
-        target_state_name = "GOsignal";
+        target_state_name = 'GOsignal';
     otherwise
         fprintf('Invalid target_state value: %d\n', target_state);
         % You might want to handle the case when target_state is neither 6 nor 4
 end
-target_state_name_char = char(target_state_name);
-%safe_target_state_name = strrep(target_state_name, ' ', '_'); % Replace spaces with underscores
 
 
-windowAroundEvent = 0.5; % sec  - to setting:  epoch to take around the trigger event
-columnsNumberBasedOnWindow = windowAroundEvent*2*1000; % windowAroundEvent in ms
+columnsNumberBasedOnWindow = settings.windowAroundEvent*2*1000; % windowAroundEvent in ms
 
 num_units = size(population, 2);
 
@@ -36,11 +33,14 @@ for u = 1:num_units
     num_trial = size (population(u).trial, 2 );
     
     % Initialize cell arrays
-    raster_labels.condition = cell(1, num_trial);
-    raster_labels.stimulus_side = cell(1, num_trial);
-    %raster_labels.stimulus_position = cell(1, num_trial);
+    raster_labels.trial_type = cell(1, num_trial);
+    raster_labels.sideSelected = cell(1, num_trial);
+    raster_labels.trial_type_side = cell(1, num_trial);
     raster_labels.stimulus_position_X_coordinate = cell(1, num_trial);
     raster_labels.stimulus_position_X_coordinate = cell(1, num_trial);
+    raster_labels.perturbation = cell(1, num_trial);
+    raster_labels.block = cell(1, num_trial);
+    raster_labels.run = cell(1, num_trial);
     
     % Initialize numeric array for raster_data
     raster_data = NaN(num_trial, columnsNumberBasedOnWindow);
@@ -59,76 +59,65 @@ for u = 1:num_units
             fprintf('State %d not found. Excluding from analysis.\n', target_state);
         else
             onsetTimeOfRequiredStage (t) = population(u).trial(t).states_onset(state_index); % Use the index to retrieve the corresponding value from 'states_onset'
-            raster_data(t, :) = histcounts(population(u).trial(t).arrival_times, (onsetTimeOfRequiredStage(t) - windowAroundEvent):0.001:(onsetTimeOfRequiredStage(t) + windowAroundEvent));
+            raster_data(t, :) = histcounts(population(u).trial(t).arrival_times, (onsetTimeOfRequiredStage(t) - settings.windowAroundEvent):0.001:(onsetTimeOfRequiredStage(t) + settings.windowAroundEvent));
         end
         
         
         %%% raster_labels
-        raster_labels.condition {1, t} = population(u).trial(t).choice;
-        if real(population(u).trial(t).tar_pos) > 0 % Convert positive values to 'R' and negative values to 'L'
-            raster_labels.stimulus_side{1, t} = 'R';
+        if population(u).trial(t).choice
+            raster_labels.trial_type{1, t} =  'choice';
         else
-            raster_labels.stimulus_side{1, t} = 'L';
+            raster_labels.trial_type{1, t} =  'instr';
         end
+        
+        if real(population(u).trial(t).tar_pos) > 0 % Convert positive values to 'R' and negative values to 'L'
+            raster_labels.sideSelected{1, t} = 'R';
+        else
+            raster_labels.sideSelected{1, t} = 'L';
+        end
+        
+        raster_labels.trial_type_side {1, t} = append(raster_labels.trial_type{1, t},'_',raster_labels.sideSelected{1, t});
         
         X_coordinate = real(population(u).trial(t).tar_pos);
         Y_coordinate = imag(population(u).trial(t).tar_pos);
         raster_labels.stimulus_position_X_coordinate{1, t} = X_coordinate;
         raster_labels.stimulus_position_Y_coordinate{1, t} = Y_coordinate;
         
-        
-        
- 
+        raster_labels.perturbation{1, t} = population(u).trial(t).perturbation;
+        raster_labels.block{1, t} = population(u).trial(t).block;
+        raster_labels.run{1, t} = population(u).trial(t).run;
         
     end
     
     %%% raster_site_info
+    raster_site_info.recording_channel = population(u).channel;
+    raster_site_info.session_ID = population(u).unit_ID(1:12);
+    raster_site_info.unit_ID = population(u).unit_ID;
+    raster_site_info.block_unit = [population(u).block_unit{:}];
+    raster_site_info.perturbation_site = population(u).perturbation_site;
+    raster_site_info.SNR_rating = population(u).SNR_rating;
+    raster_site_info.Single_rating = population(u).Single_rating;
+    raster_site_info.stability_rating = population(u).stability_rating;
+    raster_site_info.site_ID = population(u).site_ID;
+    raster_site_info.target = population(u).target;
+    raster_site_info.grid_x = population(u).grid_x;
+    raster_site_info.grid_y = population(u).grid_y;
+    raster_site_info.electrode_depth = population(u).electrode_depth;
     
-  raster_site_info.recording_channel = population(u).channel;
-    
-    % Initialize arrays to store session_ID and unit_ID as doubles
-    session_ID = zeros(1, length(population));
-    unit_ID = zeros(1, length(population));
     
     
-    % Loop through each element in the population structure
-    for i = 1:length(population)
-        % Split the unit_ID string using '_' as the delimiter
-        parts = strsplit(population(i).unit_ID, '_');
-        
-        % Extract the relevant information and convert to double
-        session_ID(i) = str2double(parts{2}(4:end)); % Extract numeric part, convert to double
-        unit_ID(i) = str2double(parts{3});           % Extract numeric part, convert to double
-    end
-
-
-%     raster_site_info.session_ID = ''; % Initialize the session_ID variable
-%     common_part_length = 12; % Specify the length of the common part % Adjust this based on your actual data
-%     for  i = 1:numel(population)% Extract the common part and assign it to session_ID
-%         if numel(population(u).unit_ID) >= common_part_length
-%             raster_site_info.session_ID = population(u).unit_ID(1:common_part_length);
-%             break;
-%         end
-%     end 
-
-%     
-%     raster_site_info = struct('unit_ID', {}); % Initialize raster_site_info structure
-%      for i = 1:numel(population)% Extract the last two characters from each unit_ID and store in raster_site_info
-%         lastTwoCharacters = population(u).unit_ID(end-1:end); % Extract last two characters from unit_ID
-%         raster_site_info(u).unit_ID = lastTwoCharacters; % Store in raster_site_info
-%      end
-    
-    %raster_site_info.unit_ID = population(u).unit_ID;
-    raster_site_info.block_unit = population(u).block_unit;
-
     raster_data = raster_data(~isnan(raster_data(:, 1)), :); % Remove NaN rows (trials with success == 0) from raster_data
-    raster_labels.condition = raster_labels.condition(~cellfun('isempty', raster_labels.condition)); % Remove empty cells (trials with success == 0) from raster_data
-    raster_labels.stimulus_side = raster_labels.stimulus_side(~cellfun('isempty', raster_labels.stimulus_side));
+    raster_labels.trial_type = raster_labels.trial_type(~cellfun('isempty', raster_labels.trial_type)); % Remove empty cells (trials with success == 0) from raster_data
+    raster_labels.sideSelected = raster_labels.sideSelected(~cellfun('isempty', raster_labels.sideSelected));
+    raster_labels.trial_type_side = raster_labels.trial_type_side(~cellfun('isempty', raster_labels.trial_type_side));
     raster_labels.stimulus_position_X_coordinate = raster_labels.stimulus_position_X_coordinate(~cellfun('isempty', raster_labels.stimulus_position_X_coordinate));
     raster_labels.stimulus_position_Y_coordinate = raster_labels.stimulus_position_Y_coordinate(~cellfun('isempty', raster_labels.stimulus_position_Y_coordinate));
+    raster_labels.perturbation = raster_labels.perturbation(~cellfun('isempty', raster_labels.perturbation));
+    raster_labels.block = raster_labels.block(~cellfun('isempty', raster_labels.block));
+    raster_labels.run = raster_labels.run(~cellfun('isempty', raster_labels.run));
     
-   
-    filename = [OUTPUT_PATH_raster population(u).unit_ID '_trial_state_' target_state_name_char '.mat'];
+    
+    filename = [OUTPUT_PATH_raster population(u).unit_ID '_raster_trial_state_' target_state_name '.mat'];
     save(filename,'raster_data', 'raster_labels', 'raster_site_info')
 end
 
--- sdndt_Sim_LIP_dPul_NDT_settings.m
+++ sdndt_Sim_LIP_dPul_NDT_settings.m
@@ -2,8 +2,28 @@
 
 % for creating raster_data
 INPUT_PATH = 'Y:\Projects\Simultaneous_dPul_PPC_recordings\ephys\dPul_LIP_Lin_20211109\';
-OUTPUT_PATH_raster = 'C:\Projects\Sim_dPul_LIP\NDT\';
+OUTPUT_PATH_raster = 'C:\Projects\Sim_dPul_LIP\NDT\raster\';
 
 
 % for creating binned_data
-OUTPUT_PATH_binned = 'C:\Projects\Sim_dPul_LIP\'
+OUTPUT_PATH_binned = 'C:\Projects\Sim_dPul_LIP\NDT\binned\';
+
+settings.windowAroundEvent = 0.5; % s:  epoch to take around the trigger event
+
+% data preparation
+settings.bin_dur = 50; % ms
+settings.smoothing_window = 3; % number of bins to smooth over
+settings.smoothing_method = 'gaussian'; % movmean / gaussian, see smoothdata
+
+
+% Decoding
+settings.num_cv_splits = 20;
+settings.num_resample_runs = 50;
+
+% plotting
+settings.time_lim = [-1000 5000]; % s, relative to cue onset
+settings.y_lim = [30 100];
+
+settings.significant_event_times = [0 200 1200]; % for plotting relevant trial events
+
+settings.errorbar_type_to_plot = 1;
