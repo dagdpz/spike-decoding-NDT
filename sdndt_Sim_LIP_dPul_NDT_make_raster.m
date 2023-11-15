a94d8ed86d458b929083a2df4a99d3783b3cd544
Mariia-Kadochnikova <kadochnikova.mariia@gmail.com>
2023-11-15 10:21:03 +0100

    Update sdndt_Sim_LIP_dPul_NDT_make_raster.m

--- sdndt_Sim_LIP_dPul_NDT_make_raster.m
+++ sdndt_Sim_LIP_dPul_NDT_make_raster.m
@@ -1,5 +1,5 @@
 function [raster_data, raster_labels, raster_site_info] = sdndt_Sim_LIP_dPul_NDT_make_raster(mat_filename)
-% [raster_data, raster_labels, raster_site_info] = Sim_LIP_dPul_NDT_make_raster('Y:\Projects\Simultaneous_dPul_PPC_recordings\ephys\dPul_LIP_Lin_20211109\population_Linus_20211109.mat');
+% [raster_data, raster_labels, raster_site_info] = sdndt_Sim_LIP_dPul_NDT_make_raster('Y:\Projects\Simultaneous_dPul_PPC_recordings\ephys\dPul_LIP_Lin_20211109\population_Linus_20211109.mat');
 
 % This code loads one population**.mat file and converts it to a raster_data, array 0 and 1.
 
@@ -27,8 +27,8 @@ target_state_name_char = char(target_state_name);
 %safe_target_state_name = strrep(target_state_name, ' ', '_'); % Replace spaces with underscores
 
 
-windowAroundEvent = 0.5; % sec  *** to setting    epoch to take around the trigger event
-some_column_size = window*2*1000; % ms
+windowAroundEvent = 0.5; % sec  - to setting:  epoch to take around the trigger event
+columnsNumberBasedOnWindow = windowAroundEvent*2*1000; % windowAroundEvent in ms
 
 num_units = size(population, 2);
 
@@ -36,14 +36,14 @@ for u = 1:num_units
     num_trial = size (population(u).trial, 2 );
     
     % Initialize cell arrays
-    raster_labels.stimulus_ID = cell(1, num_trial);
+    raster_labels.condition = cell(1, num_trial);
     raster_labels.stimulus_side = cell(1, num_trial);
-    raster_labels.stimulus_position = cell(1, num_trial);
+    %raster_labels.stimulus_position = cell(1, num_trial);
     raster_labels.stimulus_position_X_coordinate = cell(1, num_trial);
     raster_labels.stimulus_position_X_coordinate = cell(1, num_trial);
     
     % Initialize numeric array for raster_data
-    raster_data = NaN(num_trial, some_column_size);
+    raster_data = NaN(num_trial, columnsNumberBasedOnWindow);
     
     
     for t = 1:num_trial
@@ -58,13 +58,13 @@ for u = 1:num_units
         if isempty(state_index)
             fprintf('State %d not found. Excluding from analysis.\n', target_state);
         else
-            onset_value (t) = population(u).trial(t).states_onset(state_index); % Use the index to retrieve the corresponding value from 'states_onset'
-            raster_data(t, :) = histcounts(population(u).trial(t).arrival_times, (onset_value(t) - window):0.001:(onset_value(t) + window));
+            onsetTimeOfRequiredStage (t) = population(u).trial(t).states_onset(state_index); % Use the index to retrieve the corresponding value from 'states_onset'
+            raster_data(t, :) = histcounts(population(u).trial(t).arrival_times, (onsetTimeOfRequiredStage(t) - windowAroundEvent):0.001:(onsetTimeOfRequiredStage(t) + windowAroundEvent));
         end
-        %raster_data (t, :) = histcounts(population(u).trial(t).arrival_times,  (onset_value-window):0.001:(onset_value+window));
+        
         
         %%% raster_labels
-        raster_labels.stimulus_ID {1, t} = population(u).trial(t).choice;
+        raster_labels.condition {1, t} = population(u).trial(t).choice;
         if real(population(u).trial(t).tar_pos) > 0 % Convert positive values to 'R' and negative values to 'L'
             raster_labels.stimulus_side{1, t} = 'R';
         else
@@ -76,25 +76,59 @@ for u = 1:num_units
         raster_labels.stimulus_position_X_coordinate{1, t} = X_coordinate;
         raster_labels.stimulus_position_Y_coordinate{1, t} = Y_coordinate;
         
+        
+        
  
         
     end
     
     %%% raster_site_info
-    raster_site_info.session_ID = %% population(u).unit_ID; % Lin_20211109
-    raster_site_info.recording_channel = population(u).channel;
-    raster_site_info.unit_ID = population(u).unit_ID;
+    
+  raster_site_info.recording_channel = population(u).channel;
+    
+    % Initialize arrays to store session_ID and unit_ID as doubles
+    session_ID = zeros(1, length(population));
+    unit_ID = zeros(1, length(population));
+    
+    
+    % Loop through each element in the population structure
+    for i = 1:length(population)
+        % Split the unit_ID string using '_' as the delimiter
+        parts = strsplit(population(i).unit_ID, '_');
+        
+        % Extract the relevant information and convert to double
+        session_ID(i) = str2double(parts{2}(4:end)); % Extract numeric part, convert to double
+        unit_ID(i) = str2double(parts{3});           % Extract numeric part, convert to double
+    end
+
+
+%     raster_site_info.session_ID = ''; % Initialize the session_ID variable
+%     common_part_length = 12; % Specify the length of the common part % Adjust this based on your actual data
+%     for  i = 1:numel(population)% Extract the common part and assign it to session_ID
+%         if numel(population(u).unit_ID) >= common_part_length
+%             raster_site_info.session_ID = population(u).unit_ID(1:common_part_length);
+%             break;
+%         end
+%     end 
+
+%     
+%     raster_site_info = struct('unit_ID', {}); % Initialize raster_site_info structure
+%      for i = 1:numel(population)% Extract the last two characters from each unit_ID and store in raster_site_info
+%         lastTwoCharacters = population(u).unit_ID(end-1:end); % Extract last two characters from unit_ID
+%         raster_site_info(u).unit_ID = lastTwoCharacters; % Store in raster_site_info
+%      end
+    
+    %raster_site_info.unit_ID = population(u).unit_ID;
     raster_site_info.block_unit = population(u).block_unit;
 
-    
     raster_data = raster_data(~isnan(raster_data(:, 1)), :); % Remove NaN rows (trials with success == 0) from raster_data
-    raster_labels.stimulus_ID = raster_labels.stimulus_ID(~cellfun('isempty', raster_labels.stimulus_ID)); % Remove empty cells (trials with success == 0) from raster_data
+    raster_labels.condition = raster_labels.condition(~cellfun('isempty', raster_labels.condition)); % Remove empty cells (trials with success == 0) from raster_data
     raster_labels.stimulus_side = raster_labels.stimulus_side(~cellfun('isempty', raster_labels.stimulus_side));
     raster_labels.stimulus_position_X_coordinate = raster_labels.stimulus_position_X_coordinate(~cellfun('isempty', raster_labels.stimulus_position_X_coordinate));
     raster_labels.stimulus_position_Y_coordinate = raster_labels.stimulus_position_Y_coordinate(~cellfun('isempty', raster_labels.stimulus_position_Y_coordinate));
     
-    
-    filename = [OUTPUT_PATH_raster population(u).unit_ID '_trial_state_' target_state_name_char];
+   
+    filename = [OUTPUT_PATH_raster population(u).unit_ID '_trial_state_' target_state_name_char '.mat'];
     save(filename,'raster_data', 'raster_labels', 'raster_site_info')
 end
 
