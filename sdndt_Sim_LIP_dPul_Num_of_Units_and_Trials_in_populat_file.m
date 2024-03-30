5fd73a501cf98778e328c8c958a216b190bc2204
Mariia-Kadochnikova <125219601+Mariia-Kadochnikova@users.noreply.github.com>
2024-03-30 13:51:25 +0100

    30.03.2024

--- sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_in_populat_file.m
+++ sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_in_populat_file.m
@@ -1,13 +1,56 @@
-function sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_in_populat_file(injection, dateOfRecording)
+function sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_in_populat_file(injection) % , dateOfRecording
 % sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_in_populat_file('1', '20210520');
 
+
+
+%% Define typeOfSessions
+% Calculate typeOfSessions based on the injection parameter
+if strcmp(injection, '1')
+    typeOfSessions = {'left', 'right', 'all'}; % For control and injection experiments
+    %    typeOfSessions = { 'right'};
+elseif strcmp(injection, '0') || strcmp(injection, '2')
+    typeOfSessions = {''}; % For the functional interaction experiment
+else
+    error('Invalid injection value. Use ''0'', ''1'', or ''2''.');
+end
+
+
+datesForSessions = {}; % Initialize datesForSessions as an empty cell array
+if strcmp(injection, '1')
+    for type = 1:numel(typeOfSessions)
+        % Get the dates for the corresponding injection and session types
+        datesForSessions{end+1} = filelist_of_days_from_Simultaneous_dPul_PPC_recordings(injection, typeOfSessions{type});
+    end
+elseif  strcmp(injection, '0') || strcmp(injection, '2')
+    datesForSessions = filelist_of_days_from_Simultaneous_dPul_PPC_recordings(injection, typeOfSessions);
+end
+%%
+numTypesOfSessions = numel(typeOfSessions);
+
+
+for j = 1:numTypesOfSessions
+    current_type_of_session = typeOfSessions{j}
+    current_set_of_date = datesForSessions{j}; % Get the corresponding set of dates !!!!!
+    
+    for numDays = 1:numel(current_set_of_date)
+        current_date = current_set_of_date{numDays};
+        sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_internal(injection, current_type_of_session, current_date)
+    end
+
+end
+end 
+
+
+function sdndt_Sim_LIP_dPul_Num_of_Units_and_Trials_internal(injection, typeOfSessions, dateOfRecording)
 %% call additional functions
 
 % Call the function to get the dates
-allDateOfRecording = filelist_of_days_from_Simultaneous_dPul_PPC_recordings(injection);
+allDateOfRecording = filelist_of_days_from_Simultaneous_dPul_PPC_recordings(injection, typeOfSessions);
 
 % Call the settings function with the chosen set
-[base_path, INPUT_PATH, OUTPUT_PATH_raster, OUTPUT_PATH_binned, settings] = sdndt_Sim_LIP_dPul_NDT_settings(injection);
+[base_path, INPUT_PATH, OUTPUT_PATH_raster, OUTPUT_PATH_binned, settings] = sdndt_Sim_LIP_dPul_NDT_settings(injection, typeOfSessions);
+
+
 
 % Check if dateOfRecording is in the list allDateOfRecording
 if ~ismember(dateOfRecording, allDateOfRecording)
@@ -200,29 +243,83 @@ end
 
 
 %% Display
-uniqueTargets = unique({population.target}); % Extract unique targets from the entire population
-uniqueTargetsStr = ['(' strjoin(uniqueTargets, ', ') ')']; % Create a string of unique targets in round brackets
-parts = strsplit(population(1).unit_ID, '_');
-nameOfSession = strcat(parts{1}, '_', parts{2});
-disp(' '); % Display an empty line
-disp(['Number of Units for both targets ' uniqueTargetsStr ':']);
-disp(nameOfSession);
-disp(' '); % Display an empty line
+ uniqueTargets = unique({population.target}); % Extract unique targets from the entire population
+ uniqueTargetsStr = ['(' strjoin(uniqueTargets, ', ') ')']; % Create a string of unique targets in round brackets
+ parts = strsplit(population(1).unit_ID, '_');
+ nameOfSession = strcat(parts{1}, '_', parts{2});
+% disp(' '); % Display an empty line
+% disp(['Number of Units for both targets ' uniqueTargetsStr ':']);
+% disp(nameOfSession);
+% disp(' '); % Display an empty line
+% 
+% % Display the number of units for each block
+% for blockNum = uniqueBlocks
+%     field = ['block_' num2str(blockNum)];
+%     if isfield(ammount_of_units, field)
+%         disp([num2str(ammount_of_units.(field)) ' units from ' num2str(numPopulations) ' contain ' field]);
+%     end
+% end
+% 
+% disp(' '); % Display an empty line
+% disp('Number of Success Trials:');
+% disp(num_of_success_trials);
+% 
+% disp('Number of Choice Trials:');
+% disp(num_of_choice_trials);
+% 
+% disp('Number of Instructed Trials:');
+% disp(num_of_instr_trials);
 
-% Display the number of units for each block
+    
+    
+    
+% Prepare data for writing to file
+fileContent = sprintf('Number of Units for both targets %s:\n%s\n\n', uniqueTargetsStr, nameOfSession);
+
+fileContent = [fileContent sprintf('A total of %d units were recorded\n\n', numPopulations)];
+
 for blockNum = uniqueBlocks
     field = ['block_' num2str(blockNum)];
     if isfield(ammount_of_units, field)
-        disp([num2str(ammount_of_units.(field)) ' units from ' num2str(numPopulations) ' contain ' field]);
+        fileContent = [fileContent sprintf('    %s contain %d units\n', field, ammount_of_units.(field))];
     end
 end
 
-disp(' '); % Display an empty line
-disp('Number of Success Trials:');
-disp(num_of_success_trials);
 
-disp('Number of Choice Trials:');
-disp(num_of_choice_trials);
+fileContent = [fileContent sprintf('\nNumber of Success Trials:\n')];
+for blockNum = uniqueBlocks
+    field = ['block_' num2str(blockNum)];
+    if isfield(num_of_success_trials, field)
+        fileContent = [fileContent sprintf('    %s: %d\n', field, num_of_success_trials.(field))];
+    end
+end
 
-disp('Number of Instructed Trials:');
-disp(num_of_instr_trials);
+fileContent = [fileContent sprintf('\nNumber of Choice Trials:\n')];
+for blockNum = uniqueBlocks
+    field = ['block_' num2str(blockNum)];
+    if isfield(num_of_choice_trials, field)
+        fileContent = [fileContent sprintf('    %s: %d\n', field, num_of_choice_trials.(field))];
+    end
+end
+
+fileContent = [fileContent sprintf('\nNumber of Instructed Trials:\n')];
+for blockNum = uniqueBlocks
+    field = ['block_' num2str(blockNum)];
+    if isfield(num_of_instr_trials, field)
+        fileContent = [fileContent sprintf('    %s: %d\n', field, num_of_instr_trials.(field))];
+    end
+end
+
+% Define the folder and file name
+folderNameForFile = 'Num_Units_Trials_based_on_Population_file/';
+path_folderNameForFile = [OUTPUT_PATH_raster dateOfRecording '/' folderNameForFile]
+if ~exist(path_folderNameForFile, 'dir')
+    mkdir(path_folderNameForFile);
+end
+file_name = fullfile(path_folderNameForFile, ['num_of_units_and_trials_for_' dateOfRecording '.txt']);
+
+% Write the content to the file
+fileID = fopen(file_name, 'w'); % Open the file for writing
+fprintf(fileID, fileContent);
+fclose(fileID); % Close the file
+end 
